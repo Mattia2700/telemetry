167254ffb4844d8042467dcea5379ce520901f3d
Pippo98 <pippo.faccini@gmail.com>
2021-06-03 17:11:32 +0200

    Added signal ignore when reader exits from pipe

--- scripts/port/share.cpp
+++ scripts/port/share.cpp
@@ -5,18 +5,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 #include <iostream>
+
+#include <signal.h>
 
 #include "serial.h"
 
 
 using namespace std;
+
 int main()
 {
+
+  signal(SIGPIPE, SIG_IGN);
+
   int fd0;
   int fd1;
-
 
   // FIFO file path
   const char * gps0 = "/home/gps0";
@@ -29,15 +33,15 @@ int main()
     cout << "Faild opening" << endl;
     return -1;
   }
-  
+
   mkfifo(gps0, 0666);
   mkfifo(gps1, 0666);
 
   int count = 0;
 
+  string line;
   fd0 = open(gps0, O_WRONLY);
   fd1 = open(gps1, O_WRONLY);
-  string line;
   while (1)
   {
     line = to_string(count) + " " + s.read_line('\n') + "\n"; 
